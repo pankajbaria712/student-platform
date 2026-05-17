7f79436a533fe048c230cfb5d6a37488207dcfb0
Pankaj Baria <pankajbaria712@gmail.com>
2026-05-17 09:20:35 +0530

    tsconfig fixed ☑️

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b6a740df2f868b89"; }
+/******/ 		__webpack_require__.h = function() { return "3da68a34b114a87f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

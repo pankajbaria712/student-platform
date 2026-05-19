d24a4fa570e7fa21266312593f677ea0fb2ddf73
Pankaj Baria <pankajbaria712@gmail.com>
2026-05-19 08:41:01 +0530

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
       >
         {children}
         <Script src="https://checkout.razorpay.com/v1/checkout.js" />
+        <Analytics />
       </body>
     </html>
   );

9275962bb4961b3fa0bfcbf66d46cd55f7db227c
Pankaj Baria <pankajbaria712@gmail.com>
2026-05-26 20:29:30 +0530

    fixed sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,33 +1,47 @@
-export default function sitemap() {
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://gtustudenthub.vercel.app",
       lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
     },
 
     {
       url: "https://gtustudenthub.vercel.app/semester/6",
       lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
     },
 
     {
       url: "https://gtustudenthub.vercel.app/pyq/microprocessor-and-interfacing",
       lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
     },
 
     {
       url: "https://gtustudenthub.vercel.app/pyq/iot-and-applications",
       lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
     },
 
     {
       url: "https://gtustudenthub.vercel.app/pyq/data-visualization",
       lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
     },
 
     {
       url: "https://gtustudenthub.vercel.app/pyq/integrated-personality-development-course",
       lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
     },
   ];
 }
